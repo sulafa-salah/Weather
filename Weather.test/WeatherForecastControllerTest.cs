21774a2bf6e51c1e84ef37f776bc49709b8e2314
Sulafa <sulafa.ssm@gmail.com>
2024-07-18 15:52:24 +0300

    add unit testing

---
+++ Weather.test/WeatherForecastControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.API.Controllers;
+
+namespace Weather.test
+{
+    public class WeatherForecastControllerTest
+    {
+        [Fact]
+        public void GetWeatherForecast_ReturnsCorrectNumberOfForecasts()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILogger<WeatherForecastController>>();
+            var controller = new WeatherForecastController(loggerMock.Object);
+
+            // Act
+            var result = controller.Get();
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal(5, result.Count());
+        }
+    }
+}
